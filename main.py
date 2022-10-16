6b455e9286e9611c2da1f506034968ed84e43471
Ripolak <ros.jonatan98@gmail.com>
2022-10-16 15:55:26 +0300

    Add basic site generation with Jinja

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from article_generator.generate_articles import generate_articles
 from collector import collect_subreddits
 from common.db_connector import Base, db_engine, session_object
 from models.article import Article
+from site_generator.generate_site import generate_site
 
 if __name__ == '__main__':
     Base.metadata.create_all(db_engine)
-    collect_subreddits()
-    generate_articles()
-    #
-    # res = session_object.query(Article).all()
-    # print(res)
+    # collect_subreddits()
+    # generate_articles()
+    generate_site()
---
+++ site_generator/generate_site.py
@@ -0,0 +1,40 @@
+import jinja2
+
+
+# JINJA TEMPLATE:
+
+# Main: main_article_title, main_article_text, main_article_image
+# Sub articles =
+
+
+def generate_site():
+    env = jinja2.Environment()
+    template_file_content = open('site_generator/template.html').read()
+    template = env.from_string(template_file_content)
+
+    main_article = {
+        'title': 'Main article title',
+        'text': 'Main article text',
+        'image': 'https://4.img-dpreview.com/files/p/articles/5081755051/0652566517.jpeg',
+        "author": "John Smith",
+        "url": "https://www.google.com",
+    }
+    sub_articles = [
+        {
+            'title': 'Sub article title',
+            'text': 'Sub article text',
+            'image': 'https://4.img-dpreview.com/files/p/articles/5081755051/0652566517.jpeg',
+            "author": "John Smith",
+            "url": "https://www.google.com",
+        },
+        {
+            'title': 'Sub article title',
+            'text': 'Sub article text',
+            'image': 'https://4.img-dpreview.com/files/p/articles/5081755051/0652566517.jpeg',
+            "author": "John Smith",
+            "url": "https://www.google.com",
+        },
+    ]
+
+    output = template.render(main_article=main_article, sub_articles=sub_articles)
+    open('site_generator/output.html', 'w').write(output)
