e7452a3ca50ecec81c603f0497c31d99a3052fc5
Ripolak <ros.jonatan98@gmail.com>
2022-10-29 11:20:28 +0300

    Add basic site generation

--- article_chooser/choose_articles.py
+++ article_chooser/choose_articles.py
@@ -2,7 +2,7 @@ from typing import List
 
 from common.db_connector import session_object
 from models.db.article import Article
-from models.website.website import Website
+from models.website.website import Website, NewsRow
 
 
 def sort_articles_by_upvotes(articles: List[Article]) -> List[Article]:
@@ -17,19 +17,23 @@ def get_articles_for_subreddit(subreddit: str, articles: List[Article]) -> List[
     return [article for article in articles if article.post.subreddit == subreddit]
 
 
-# def generate_sub_articles(sorted_articles: List[Article]) -> List[Article]:
-#     sorted_articles = sort_articles_by_upvotes(articles)
-#     used_articles = []
-#     main_article = sorted_articles[0]
-#     sub_articles = sorted_articles[1:4]
-#     mini_articles = sorted_articles[4:8]
-#     used_articles.append(main_article)
-#     used_articles.extend(sub_articles)
-#     used_articles.extend(mini_articles)
+def generate_news_rows(articles: List[Article], website: Website):
+    subreddits = get_list_of_subreddits_with_articles(articles)
+    for subreddit in subreddits:
+        subreddit_articles = get_articles_for_subreddit(subreddit, articles)
+        unused_subreddit_articles = [article for article in
+                                     subreddit_articles if not website.is_article_in_website(article)]
+        unused_subreddit_articles = sort_articles_by_upvotes(unused_subreddit_articles)
+        news_row = NewsRow(name=subreddit, articles=unused_subreddit_articles[:3])
+        website.news_rows.append(news_row)
 
 
-def choose_articles():
+def choose_articles() -> Website:
     current_articles: List[Article] = session_object.query(Article).all()  # TODO: Don't fetch all articles
     sorted_articles = sort_articles_by_upvotes(current_articles)
     main_article = sorted_articles[0]
     website = Website(main_article=main_article, sub_articles=[], mini_articles=[], news_rows=[])
+    website.sub_articles = sorted_articles[1:4]
+    website.mini_articles = sorted_articles[4:8]
+    generate_news_rows(sorted_articles, website)
+    return website
