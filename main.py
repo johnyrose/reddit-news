1b037d6b71909c9a3312745e3b00d22f68778399
Ripolak <ros.jonatan98@gmail.com>
2022-11-03 23:35:48 +0200

    Add warning is sqlite file already exists

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import os
 from article_chooser.choose_articles import choose_articles
 from article_generator.generate_articles import generate_articles
 from collector import collect_subreddits
 from common.logger import logger
 from common.db_connector import Base, db_engine
-from common.settings import PERFORM_DATA_COLLECTION, PERFORM_WEBSITE_GENERATION
+from common.settings import PERFORM_DATA_COLLECTION, PERFORM_WEBSITE_GENERATION, DB_FILE
 from site_generator.generate_site import generate_site
 
-if __name__ == '__main__':
+
+def check_for_existing_db_file():
+    if os.path.exists(DB_FILE):
+        logger.warning(f'SQLITE file {DB_FILE} already exists. This might cause duplicates or older articles to'
+                       f' appear. If you want a clean slate, it is recommended to delete this file and run the '
+                       f'process again.')
+
+
+def main():
     Base.metadata.create_all(db_engine)
     if PERFORM_DATA_COLLECTION:
+        check_for_existing_db_file()
         logger.info('Collecting subreddits...')
         collect_subreddits()
         logger.info('Generating articles...')
@@ -22,3 +32,7 @@ if __name__ == '__main__':
         generate_site(website)
     else:
         logger.info('Website generation is disabled, skipping...')
+
+
+if __name__ == '__main__':
+    main()
